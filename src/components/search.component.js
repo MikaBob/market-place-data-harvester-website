d949c95b5fc6b67995f6ee4c8eba441fd5e0c794
Bob <pji-2bke@hotmail.fr>
2019-08-29 20:23:41 +0200

    Add item's page with graph, Webpack+Babel. Modifiy price Model and Route.

--- src/components/search.component.js
+++ src/components/search.component.js
@@ -10,44 +10,44 @@ const ItemRow = props => (
         <td>{props.item.type}</td>
         <td>{props.item.category}</td>
         <td>
-            <Link to={"/item/"+props.item.itemGID}>See prices</Link>
+            <Link to={"/item/" + props.item.itemGID}>See prices</Link>
         </td>
     </tr>
 )
 
 export default class Search extends Component {
-        
+
     constructor(props) {
         super(props);
 
-        this.onChangeSearch = this.onChangeSearch.bind(this);
+        this.onChangeSearch = this.onChangeSearch.bind(this); // binding "this" to the class. Otherwise "this" won't be defined inside the function
 
         this.state = {
-          itemName: '',
-          items: []
+            itemName: '',
+            items: []
         };
     }
-    
+
     onChangeSearch(event) {
-        this.setState({ itemName: event.target.value }, () => {
-            if(this.state.itemName.length > 2){
-                axios.get('http://localhost:8090/item/list', { params: { itemName: this.state.itemName }})
-                    .then(response => {
-                        this.setState({ items: response.data });
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
+        this.setState({itemName: event.target.value}, () => {
+            if (this.state.itemName.length > 2) {
+                axios.get(process.env.API_URL + '/item/list', {params: {itemName: this.state.itemName}})
+                        .then(response => {
+                            this.setState({items: response.data});
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
             }
         }); // setState is async
     }
-    
+
     refreshItemList() {
         return this.state.items.map(item => {
             return <ItemRow item={item} key={item.itemGID}/>;
         });
     }
-    
+
     render() {
         return (
                 <div>
@@ -71,6 +71,6 @@ export default class Search extends Component {
                         </table>
                     </div>
                 </div>
-        );
+                );
     }
 }
